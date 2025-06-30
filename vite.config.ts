f64cdca94b0d042604e0f6428fbb22047cca7de5
Salvatore Ficara <ficaras0@gmail.com>
2025-06-30 19:20:34 -0400

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  publicDir: 'public', // Ensure this points to your public folder
+  build: {
+    outDir: 'dist',
+    // Make sure _redirects gets copied
+  }
 })
